f9a7fee18883d83c4b74a00f2a75ba2e56900258
lv 周 <lvzhou@lvdeMacBook-Pro.local>
2022-04-02 23:07:45 +0800

    fix signal Interrupt

--- http_bench.go
+++ http_bench.go
@@ -1,129 +1,86 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
-	"net/http"
-	gourl "net/url"
-	"regexp"
-	"runtime"
-	"strings"
-	"sort"
-	"crypto/tls"
 	"io"
-	"bufio"
 	"io/ioutil"
+	"net/http"
 	"net/url"
+	gourl "net/url"
 	"os"
 	"os/signal"
+	"regexp"
+	"runtime"
+	"sort"
+	"strings"
 	"sync"
 	"time"
-	"math/rand"
 )
 
 const (
-	barChar = "∎"
+	CMD_START = 0x01
+	CMD_STOP  = 0x02
 )
 
-var benchRequestUrl map[string]int = make(map[string]int)
-var command chan string
+type (
+	Report struct {
+		avgTotal float64
+		fastest  float64
+		slowest  float64
+		average  float64
+		rps      float64
 
-type Report struct {
-	avgTotal float64
-	fastest  float64
-	slowest  float64
-	average  float64
-	rps      float64
+		results chan *result
+		total   time.Duration
 
-	results chan *result
-	total   time.Duration
+		errorDist      map[string]int
+		statusCodeDist map[int]int
+		lats           []float64
+		sizeTotal      int64
 
-	errorDist      map[string]int
-	statusCodeDist map[int]int
-	lats           []float64
-	sizeTotal      int64
-
-	output string
-
-	req			   *BenchRequest
-}
-
-type BenchRequest struct {
-	http.Request
-	BRequest 	[]*http.Request
-	BUrl 		[]string
-	BReport 	*Report
-
-	BReportUrls []string
-}
-
-func (b * BenchRequest) getRandomRequest() *http.Request {
-	rint := rand.Intn(len(b.BRequest))
-	url := b.BUrl[rint]
-	b.BReportUrls = append(b.BReportUrls, url)
-	command <- url
-	return b.BRequest[rint]
-}
-
-func (b * BenchRequest) newReport(size int, results chan *result, 
-	output string, total time.Duration) *Report {
-	if b.BReport == nil {
-		b.BReport = &Report{
-			output:         output,
-			results:        results,
-			total:          total,
-			statusCodeDist: make(map[int]int),
-			errorDist:      make(map[string]int),
-			req:			b,
-		}
+		output string
 	}
 
-	return b.BReport
+	headerSlice []string
+
+	BenchParameters struct {
+		// Commands, eg: "START、STOP、RESULT".
+		Cmd int `json:"cmd"`
+		// Request Method.
+		RequestMethod string `json:"request_method"`
+		// Request Body.
+		RequestBody string `json:"request_body"`
+		// N is the total number of requests to make.
+		N int `json:"n"`
+		// C is the concurrency level, the number of concurrent workers to run.
+		C int `json:"c"`
+		// Timeout in seconds.
+		Timeout int `json:"timeout"`
+		// Qps is the rate limit.
+		Qps int `json:"qps"`
+		// DisableCompression is an option to disable compression in response
+		DisableCompression bool `json:"disable_compression"`
+		// DisableKeepAlives is an option to prevents re-use of TCP connections between different HTTP requests
+		DisableKeepAlives bool `json:"disable_keepalives"`
+		// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
+		ProxyAddr string `json:"proxy_addr"`
+		// Basic authentication, username:password.
+		AuthUsername string              `json:"auth_username"`
+		AuthPassword string              `json:"auth_password"`
+		Headers      map[string][]string `json:"headers"`
+		Urls         []string            `json:"urls"`
+	}
+)
+
+func (h *headerSlice) String() string {
+	return fmt.Sprintf("%s", *h)
 }
 
-func parseUrlsFile(fname string) ([]string, error) {
-	var retArr []string
-	f, err := os.Open(fname)
-	if err != nil {
-		return retArr, err
-	}
-
-	buf := bufio.NewReader(f)
-	for {
-		line, err := buf.ReadString('\n')
-		line = strings.TrimSpace(line)
-		if err != nil {
-			if err == io.EOF {
-				err = nil
-			}
-			return retArr, err
-		}
-		retArr = append(retArr, line)
-	}
-
-	return retArr, nil
-}
-
-func newRequest(method string, urlstr []string, body io.Reader) (*BenchRequest, error) {
-	breq := &BenchRequest{}
-	if len(urlstr) > 1 {
-		for _, url := range urlstr {
-			req, err := http.NewRequest(method, url, body)
-			if err != nil {
-				continue
-			}
-			breq.BRequest = append(breq.BRequest, req)
-			breq.BUrl = append(breq.BUrl, url)
-		}
-
-		return breq, nil
-	} else {
-		req, err := http.NewRequest(method, urlstr[0], body)
-		breq.BRequest = append(breq.BRequest, req)
-		breq.BUrl = append(breq.BUrl, urlstr[0])
-
-		return breq, err
-	}
+func (h *headerSlice) Set(value string) error {
+	*h = append(*h, value)
+	return nil
 }
 
 func (r *Report) finalize() {
@@ -152,19 +109,18 @@ func (r *Report) finalize() {
 func (r *Report) print() {
 	sort.Float64s(r.lats)
 
-	if r.output == "csv" {
+	switch r.output {
+	case "csv":
 		r.printCSV()
 		return
+	default:
+		// pass
 	}
 
 	if len(r.lats) > 0 {
 		r.fastest = r.lats[0]
 		r.slowest = r.lats[len(r.lats)-1]
-		fmt.Printf("Requests:\n")
-		for k, v := range benchRequestUrl {
-			fmt.Printf("  [%d] %s\n", v, k)
-		}
-		
+
 		fmt.Printf("\nSummary:\n")
 		fmt.Printf("  Total:\t%4.4f secs\n", r.total.Seconds())
 		fmt.Printf("  Slowest:\t%4.4f secs\n", r.slowest)
@@ -176,9 +132,6 @@ func (r *Report) print() {
 			fmt.Printf("  Size/request:\t%d bytes\n", r.sizeTotal/int64(len(r.lats)))
 		}
 		r.printStatusCodes()
-
-		// r.printHistogram()
-
 		r.printLatencies()
 	}
 
@@ -189,7 +142,7 @@ func (r *Report) print() {
 
 func (r *Report) printCSV() {
 	for i, val := range r.lats {
-		fmt.Printf("%v,%4.4f,%s\n", i+1, val, r.req.BReportUrls[i])
+		fmt.Printf("%v,%4.4f\n", i+1, val)
 	}
 }
 
@@ -212,39 +165,6 @@ func (r *Report) printLatencies() {
 	}
 }
 
-func (r *Report) printHistogram() {
-	bc := 10
-	buckets := make([]float64, bc+1)
-	counts := make([]int, bc+1)
-	bs := (r.slowest - r.fastest) / float64(bc)
-	for i := 0; i < bc; i++ {
-		buckets[i] = r.fastest + bs*float64(i)
-	}
-	buckets[bc] = r.slowest
-	var bi int
-	var max int
-	for i := 0; i < len(r.lats); {
-		if r.lats[i] <= buckets[bi] {
-			i++
-			counts[bi]++
-			if max < counts[bi] {
-				max = counts[bi]
-			}
-		} else if bi < len(buckets)-1 {
-			bi++
-		}
-	}
-	fmt.Printf("\nResponse time histogram:\n")
-	for i := 0; i < len(buckets); i++ {
-		// Normalize bar lengths.
-		var barLen int
-		if max > 0 {
-			barLen = counts[i] * 40 / max
-		}
-		fmt.Printf("  %4.3f [%v]\t|%v\n", buckets[i], counts[i], strings.Repeat(barChar, barLen))
-	}
-}
-
 // Prints status code distribution.
 func (r *Report) printStatusCodes() {
 	fmt.Printf("\nStatus code distribution:\n")
@@ -260,137 +180,130 @@ func (r *Report) printErrors() {
 	}
 }
 
-type result struct {
-	err           error
-	statusCode    int
-	duration      time.Duration
-	contentLength int64
-}
+type (
+	result struct {
+		err           error
+		statusCode    int
+		duration      time.Duration
+		contentLength int64
+	}
 
-type Bencher struct {
-	// Request is the request to be made.
-	Request *BenchRequest
-	RequestBody string
-	// N is the total number of requests to make.
-	N int
-	// C is the concurrency level, the number of concurrent workers to run.
-	C int
-	// Timeout in seconds.
-	Timeout int
-	// Qps is the rate limit.
-	Qps int
-	// DisableCompression is an option to disable compression in response
-	DisableCompression bool
-	// DisableKeepAlives is an option to prevents re-use of TCP connections between different HTTP requests
-	DisableKeepAlives bool
-	// Output represents the output type. If "csv" is provided, the
-	// output will be dumped as a csv stream.
-	Output string
-	// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
-	// Optional.
-	ProxyAddr *url.URL
-	results chan *result
-}
+	BenchProducer struct {
+		RequestParams *BenchParameters
+		// Output represents the output type. If "csv" is provided, the
+		// output will be dumped as a csv stream.
+		Output string
+		// ProxyAddr is the address of HTTP proxy server in the format on "host:port".
+		ProxyURL *url.URL
+		results  chan *result
+		signal   chan os.Signal
+		wg       sync.WaitGroup
+	}
+)
 
-func (b *Bencher) run() {
-	b.results = make(chan *result, b.N)
+func (b *BenchProducer) Start() {
+	var wg sync.WaitGroup
+
+	b.results = make(chan *result, b.RequestParams.N)
 
 	start := time.Now()
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	b.signal = make(chan os.Signal, 1)
+	signal.Notify(b.signal, os.Interrupt)
+
+	wg.Add(2)
 
 	go func() {
-		<-c
-		b.Request.newReport(b.N, b.results, b.Output, time.Now().Sub(start)).finalize()
-		os.Exit(1)
+		<-b.signal
+		b.RequestParams.Cmd = CMD_STOP
+		close(b.results)
+		wg.Done()
+		wg.Wait()
+	}()
+
+	go func() {
+		b.newReport(time.Now().Sub(start)).finalize()
+		wg.Done()
 	}()
 
 	b.runWorkers()
-	b.Request.newReport(b.N, b.results, b.Output, time.Now().Sub(start)).finalize()
-	close(b.results)
 }
 
-func (b *Bencher) makeRequest(c *http.Client) {
-	s := time.Now()
-	var size int64
-	var code int
-
-	resp, err := c.Do(cloneRequest(b.Request.getRandomRequest(), b.RequestBody))
-	if err == nil {
-		size = resp.ContentLength
-		code = resp.StatusCode
-		io.Copy(ioutil.Discard, resp.Body)
-		resp.Body.Close()
-	}
-	
-	b.results <- &result{
-		statusCode:    code,
-		duration:      time.Now().Sub(s),
-		err:           err,
-		contentLength: size,
-	}
-}
-
-func (b *Bencher) runWorker(n int) {
+func (b *BenchProducer) runWorker(n int) {
 	var throttle <-chan time.Time
-	if b.Qps > 0 {
-		throttle = time.Tick(time.Duration(1e6/(b.Qps)) * time.Microsecond)
+	if b.RequestParams.Qps > 0 {
+		throttle = time.Tick(time.Duration(1e6/(b.RequestParams.Qps)) * time.Microsecond)
 	}
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{
 			InsecureSkipVerify: true,
 		},
-		DisableCompression: b.DisableCompression,
-		DisableKeepAlives:  b.DisableKeepAlives,
-		TLSHandshakeTimeout: time.Duration(b.Timeout) * time.Millisecond,
-		Proxy:               http.ProxyURL(b.ProxyAddr),
+		DisableCompression:  b.RequestParams.DisableCompression,
+		DisableKeepAlives:   b.RequestParams.DisableKeepAlives,
+		TLSHandshakeTimeout: time.Duration(b.RequestParams.Timeout) * time.Millisecond,
+		Proxy:               http.ProxyURL(b.ProxyURL),
+		TLSNextProto:        make(map[string]func(string, *tls.Conn) http.RoundTripper),
 	}
 
-	tr.TLSNextProto = make(map[string]func(string, *tls.Conn) http.RoundTripper)
-	
 	client := &http.Client{Transport: tr}
-	for i := 0; i < n; i++ {
-		if b.Qps > 0 {
+	for i := 0; i < n && b.RequestParams.Cmd != CMD_STOP; i++ {
+		if b.RequestParams.Qps > 0 {
 			<-throttle
 		}
-		b.makeRequest(client)
+
+		var t = time.Now()
+		var size int64
+		var code int
+
+		resp, err := client.Do(b.getRequest())
+		if err == nil {
+			size = resp.ContentLength
+			code = resp.StatusCode
+			io.Copy(ioutil.Discard, resp.Body)
+			resp.Body.Close()
+		}
+
+		b.results <- &result{
+			statusCode:    code,
+			duration:      time.Now().Sub(t),
+			err:           err,
+			contentLength: size,
+		}
 	}
 }
 
-func (b *Bencher) runWorkers() {
+func (b *BenchProducer) runWorkers() {
+	// Ignore the case where b.RequestParams.N % b.RequestParams.C != 0.
 	var wg sync.WaitGroup
-	wg.Add(b.C)
-
-	// Ignore the case where b.N % b.C != 0.
-	for i := 0; i < b.C; i++ {
+	for i := 0; i < b.RequestParams.C && b.RequestParams.Cmd != CMD_STOP; i++ {
+		wg.Add(1)
 		go func() {
-			b.runWorker(b.N / b.C)
+			b.runWorker(b.RequestParams.N / b.RequestParams.C)
 			wg.Done()
 		}()
 	}
 	wg.Wait()
 }
 
-// cloneRequest returns a clone of the provided *http.Request.
-// The clone is a shallow copy of the struct and its Header map.
-func cloneRequest(r *http.Request, body string) *http.Request {
+func (b *BenchProducer) getRequest() *http.Request {
 	// shallow copy of the struct
 	r2 := new(http.Request)
-	*r2 = *r
-	// deep copy of the Header
-	r2.Header = make(http.Header, len(r.Header))
-	for k, s := range r.Header {
-		r2.Header[k] = append([]string(nil), s...)
-	}
-	r2.Body = ioutil.NopCloser(strings.NewReader(body))
 	return r2
+}
+
+func (b *BenchProducer) newReport(total time.Duration) *Report {
+	return &Report{
+		output:         b.Output,
+		results:        b.results,
+		total:          total,
+		statusCodeDist: make(map[int]int),
+		errorDist:      make(map[string]int),
+	}
 }
 
 func usageAndExit(msg string) {
 	if msg != "" {
-		fmt.Fprintf(os.Stderr, msg)
-		fmt.Fprintf(os.Stderr, "\n\n")
+		fmt.Fprintf(os.Stderr, msg+"\n\n")
 	}
 	flag.Usage()
 	fmt.Fprintf(os.Stderr, "\n")
@@ -407,37 +320,44 @@ func parseInputWithRegexp(input, regx string) ([]string, error) {
 	return matches, nil
 }
 
-type headerSlice []string
+func parseUrlsFile(fname string) ([]string, error) {
+	var retArr []string
+	if file, err := os.Open(fname); err != nil {
+		return retArr, err
+	} else {
+		if content, err := ioutil.ReadAll(file); err != nil {
+			return retArr, err
+		} else {
+			arr := strings.FieldsFunc(string(content), func(r rune) bool {
+				return r == '\n' || r == '\r' || r == ' '
+			})
+			for _, v := range arr {
+				if len(v) > 0 {
+					retArr = append(retArr, v)
+				}
+			}
+		}
+	}
 
-func (h *headerSlice) String() string {
-	return fmt.Sprintf("%s", *h)
-}
-
-func (h *headerSlice) Set(value string) error {
-	*h = append(*h, value)
-	return nil
+	return retArr, nil
 }
 
 var (
-	headerslice headerSlice
+	params BenchParameters
 
 	headerRegexp = `^([\w-]+):\s*(.+)`
 	authRegexp   = `^(.+):([^\s].+)`
 
-	m           = flag.String("m", "GET", "")
-	headers     = flag.String("h", "", "")
-	body        = flag.String("d", "", "")
-	accept      = flag.String("A", "", "")
-	contentType = flag.String("T", "text/html", "")
-	authHeader  = flag.String("a", "", "")
-	hostHeader  = flag.String("host", "", "")
+	m          = flag.String("m", "GET", "")
+	body       = flag.String("d", "", "")
+	authHeader = flag.String("a", "", "")
 
-	output = flag.String("o", "", "")
+	output = flag.String("o", "", "") // Output type
 
-	c = flag.Int("c", 50, "")
-	n = flag.Int("n", 200, "")
-	q = flag.Int("q", 0, "")
-	t = flag.Int("t", 0, "")
+	c = flag.Int("c", 50, "")  // Number of requests to run concurrently
+	n = flag.Int("n", 200, "") // Number of requests to run
+	q = flag.Int("q", 0, "")   // Rate limit, in seconds (QPS)
+	t = flag.Int("t", 0, "")   // Timeout in ms
 
 	cpus = flag.Int("cpus", runtime.GOMAXPROCS(-1), "")
 
@@ -445,7 +365,7 @@ var (
 	disableKeepAlives  = flag.Bool("disable-keepalive", false, "")
 	proxyAddr          = flag.String("x", "", "")
 
-	urls = flag.String("file", "", "")
+	urlFile = flag.String("file", "", "")
 )
 
 var usage = `Usage: go_bench [options...] <url>
@@ -454,6 +374,7 @@ Options:
   -c  Number of requests to run concurrently. Total number of requests cannot
       be smaller than the concurency level.
   -q  Rate limit, in seconds (QPS).
+  -t  Timeout in ms.
   -o  Output type. If none provided, a summary is printed.
       "csv" is the only supported alternative. Dumps the response
       metrics in comma-seperated values format.
@@ -461,10 +382,6 @@ Options:
   -H  Custom HTTP header. You can specify as many as needed by repeating the flag.
 	  for example, -H "Accept: text/html" -H "Content-Type: application/xml", 
 	  but "Host: ***", replace that with -host.
-  -t  Timeout in ms.
-  -A  HTTP Accept header.
-  -d  HTTP request body.
-  -T  Content-type, defaults to "text/html".
   -a  Basic authentication, username:password.
   -x  HTTP Proxy address as host:port.
   -disable-compression  Disable compression.
@@ -472,12 +389,11 @@ Options:
                         connections between different HTTP requests.
   -cpus                 Number of used cpu cores.
                         (default for current machine is %d cores)
-  -host                 HTTP Host header.
   -file  Request url file, a launch request in the random selection file
 Example:
-  ./go_bench -n 1000 -c 10 -t 3000 -m GET http://127.0.0.1/test1
+  ./http_bench -n 1000 -c 10 -t 3000 -m GET http://127.0.0.1/test1
   or
-  ./go_bench -n 1000 -c 10 -t 3000 -m GET -file urls.txt
+  ./http_bench -n 1000 -c 10 -t 3000 -m GET -file urls.txt
 
 Notice:
   urls.txt format like this:
@@ -486,129 +402,80 @@ Notice:
 `
 
 func main() {
-	defer func() {
-		close(command)
-	}()
-
-	command = make(chan string, 1000)
-	go func() {
-		for {
-			select{
-			case c := <-command:
-				if _, ok := benchRequestUrl[c]; ok {
-					benchRequestUrl[c]++
-				} else {
-					benchRequestUrl[c] = 1
-				}
-			default:
-				break
-			}
-		}
-	}()
-
 	flag.Usage = func() {
 		fmt.Fprint(os.Stderr, fmt.Sprintf(usage, runtime.NumCPU()))
 	}
 
-	flag.Var(&headerslice, "H", "")
+	var headerslice headerSlice
+	flag.Var(&headerslice, "H", "") // Custom HTTP header
 	flag.Parse()
 
-	if flag.NArg() < 1 && *urls == "" {
+	if flag.NArg() < 1 {
 		usageAndExit("")
 	}
 
 	runtime.GOMAXPROCS(*cpus)
-	num := *n
-	conc := *c
-	q := *q
+	params.N = *n
+	params.C = *c
+	params.Qps = *q
 
-	if num <= 0 || conc <= 0 {
+	if params.N <= 0 || params.C <= 0 {
 		usageAndExit("n and c cannot be smaller than 1.")
 	}
 
-	if num < conc {
+	if params.N < params.C {
 		usageAndExit("n cannot be less than c")
 	}
 
-	var urlArr []string
-	if *urls == "" {
-		urlArr = append(urlArr, flag.Args()[0])
+	if *urlFile == "" {
+		params.Urls = append(params.Urls, flag.Args()[0])
 	} else {
 		var err error
-		if urlArr, err = parseUrlsFile(*urls); err != nil {
-			usageAndExit("urls.txt file read error.")
+		if params.Urls, err = parseUrlsFile(*urlFile); err != nil {
+			usageAndExit(*urlFile + " file read error(" + err.Error() + ").")
 		}
 	}
-	method := strings.ToUpper(*m)
 
-	// set content-type
-	header := make(http.Header)
-	header.Set("Content-Type", *contentType)
-	// set any other additional headers
-	if *headers != "" {
-		usageAndExit("flag '-h' is deprecated, please use '-H' instead.")
-	}
+	params.RequestMethod = strings.ToUpper(*m)
+
 	// set any other additional repeatable headers
 	for _, h := range headerslice {
 		match, err := parseInputWithRegexp(h, headerRegexp)
 		if err != nil {
 			usageAndExit(err.Error())
 		}
-
-		header.Set(match[1], match[2])
-	}
-
-	if *accept != "" {
-		header.Set("Accept", *accept)
+		if params.Headers == nil {
+			params.Headers = make(map[string][]string, 0)
+		}
+		params.Headers[match[1]] = []string{match[2]}
 	}
 
 	// set basic auth if set
-	var username, password string
 	if *authHeader != "" {
-		match, err := parseInputWithRegexp(*authHeader, authRegexp)
-		if err != nil {
+		if match, err := parseInputWithRegexp(*authHeader, authRegexp); err != nil {
 			usageAndExit(err.Error())
+		} else {
+			params.AuthUsername, params.AuthPassword = match[1], match[2]
 		}
-		username, password = match[1], match[2]
 	}
 
 	if *output != "csv" && *output != "" {
 		usageAndExit("Invalid output type; only csv is supported.")
 	}
 
-	var proxyURL *gourl.URL
+	var proxyUrl *gourl.URL
 	if *proxyAddr != "" {
 		var err error
-		proxyURL, err = gourl.Parse(*proxyAddr)
-		if err != nil {
+		if proxyUrl, err = gourl.Parse(*proxyAddr); err != nil {
 			usageAndExit(err.Error())
 		}
+		params.ProxyAddr = *proxyAddr
 	}
 
-	req, err := newRequest(method, urlArr, nil)
-	if err != nil {
-		usageAndExit(err.Error())
+	p := &BenchProducer{
+		RequestParams: &params,
+		Output:        *output,
+		ProxyURL:      proxyUrl,
 	}
-	req.Header = header
-	if username != "" || password != "" {
-		req.SetBasicAuth(username, password)
-	}
-
-	// set host header if set
-	if *hostHeader != "" {
-		req.Host = *hostHeader
-	}
-
-	(&Bencher{
-		Request:            req,
-		RequestBody:        *body,
-		N:                  num,
-		C:                  conc,
-		Qps:                q,
-		Timeout:            *t,
-		DisableCompression: *disableCompression,
-		DisableKeepAlives:  *disableKeepAlives,
-		ProxyAddr:          proxyURL,
-		Output:             *output,
-	}).run()
+	p.Start()
 }
