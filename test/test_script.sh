af95168d93ad7e51c3c7602fbe03248b7017873c
352526781 <zhoulv2000@163.com>
2022-09-05 21:28:06 +0800

    fix test script

--- test/test_script.sh
+++ test/test_script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+GO=go
+
 function start_go_process() {
     echo "start process --> "$1
-    nohup go test -v $1 -args $2 2>&1 >/dev/null & echo $! > command.pid
+    nohup $GO test -v $1 -args $2 2>&1 >/dev/null & echo $! > command.pid
 }
 
 function kill_go_process() {
@@ -16,7 +18,7 @@ echo "================= single stress test http/1"
 # 1. start http1 server
 start_go_process echo_http1_test.go $listen
 # 2. start stress test
-go run ../http_bench.go -c 1 -d 2s -http http1 -m GET -url "http://$listen/"
+$GO run ../http_bench.go -c 1 -d 2s -http http1 -m GET -url "http://$listen/"
 # 3. stop http1 server
 kill_go_process
 
@@ -24,7 +26,7 @@ echo "================= single stress test http/2"
 # 1. start http2 server
 start_go_process echo_http2_test.go $listen
 # 2. start stress test
-go run ../http_bench.go -c 1 -d 2s -http http2 -m GET -url "https://$listen/"
+$GO run ../http_bench.go -c 1 -d 2s -http http2 -m GET -url "https://$listen/"
 # 3. stop http2 server
 kill_go_process
 
@@ -32,7 +34,7 @@ echo "================= single stress test http/3"
 # 1. start http3 server
 start_go_process echo_http3_test.go $listen
 # 2. start stress test
-go run ../http_bench.go -c 1 -d 2s -http http3 -m GET -url "https://$listen/"
+$GO run ../http_bench.go -c 1 -d 2s -http http3 -m GET -url "https://$listen/"
 # 3. stop http3 server
 kill_go_process
 
@@ -40,6 +42,6 @@ echo "================= single stress test ws"
 # 1. start ws server
 start_go_process echo_ws_test.go $listen
 # 2. start stress test
-go run ../http_bench.go -c 1 -d 2s -http ws -m GET -url "ws://$listen/"
+$GO run ../http_bench.go -c 1 -d 2s -http ws -m GET -url "ws://$listen/"
 # 3. stop ws server
 kill_go_process
